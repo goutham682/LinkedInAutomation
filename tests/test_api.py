9cbb6ade7d36cd8e8572f800f02cf9c9cad9dc9c
Gouthamprabhu R <gouthamprabhurit2023@jkkn.ac.in>
2026-07-07 05:51:02 +0530

    Added PUT and DELETE API test cases

--- tests/test_api.py
+++ tests/test_api.py
@@ -27,3 +27,28 @@ def test_post_api():
     data = response.json()
 
     assert data["title"] == "API Automation Test"
+def test_put_api():
+    url = "https://jsonplaceholder.typicode.com/posts/1"
+
+    payload = {
+        "id": 1,
+        "title": "Updated API Test",
+        "body": "Updating data using PUT request",
+        "userId": 1
+    }
+
+    response = requests.put(url, json=payload)
+
+    assert response.status_code == 200
+
+    data = response.json()
+
+    assert data["title"] == "Updated API Test"
+
+
+def test_delete_api():
+    url = "https://jsonplaceholder.typicode.com/posts/1"
+
+    response = requests.delete(url)
+
+    assert response.status_code == 200
