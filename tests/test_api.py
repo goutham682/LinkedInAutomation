53f5d71a7cd704acc6c72b3a2712e8fe2282c6f0
Gouthamprabhu R <gouthamprabhurit2023@jkkn.ac.in>
2026-07-07 05:46:50 +0530

    Added POST API test case

--- tests/test_api.py
+++ tests/test_api.py
@@ -11,3 +11,19 @@ def test_get_api():
 
     assert "id" in data
     assert "title" in data
+def test_post_api():
+    url = "https://jsonplaceholder.typicode.com/posts"
+
+    payload = {
+        "title": "API Automation Test",
+        "body": "Testing POST request using Python",
+        "userId": 1
+    }
+
+    response = requests.post(url, json=payload)
+
+    assert response.status_code == 201
+
+    data = response.json()
+
+    assert data["title"] == "API Automation Test"
